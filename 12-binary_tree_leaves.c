e1e696aff59ca0b8a004a6694cb6abdd71d5b511
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-07-21 19:47:00 +0000

    12-binary_tree_leaves.c

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_leaves - function that measures the height of a binary tree
+ * @tree: pointer to the root node of the tree
+ * Return: height of a binary tree or Null If tree is NULL
+ */
+
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	size_t lt = 0, rh = 0;
+
+	if (tree == NULL)
+		return (0);
+	lt = binary_tree_leaves(tree->left);
+	rh = binary_tree_leaves(tree->right);
+	if (tree->left == NULL && tree->right == NULL)
+		return (lt + rh + 1);
+	return (lt + rh);
+}
